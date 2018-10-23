5095807da71cf922bf979b1796908016802a95be
John Biddulph <john.biddulph@changingworkplace.com>
2018-10-23 15:20:58 +0100

    first commit

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export var firebasConfig = {
+    apiKey: "AIzaSyAGtx5LqMd0IfMgUWVZ7NVDLvpDrQWFpak",
+    authDomain: "quotes-cc05d.firebaseapp.com",
+    databaseURL: "https://quotes-cc05d.firebaseio.com",
+    projectId: "quotes-cc05d",
+    storageBucket: "quotes-cc05d.appspot.com",
+    messagingSenderId: "48609054717"
+  };
